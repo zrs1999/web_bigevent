282f0879d85d0915bb791b32be81656713546a41
zhurongsheng <992365244@qq.com>
2020-06-29 10:06:33 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,22 +2,21 @@
 //会先调用ajaxPrefilter这个函数
 //在这个函数中，我们可以拿到Ajax给我们提供的配置对象
 $.ajaxPrefilter((options) => {
-    options.url = "http://127.0.0.1:8080" + options.url
+    options.url = `http://127.0.0.1:8080${options.url}`
         //统一为有权限的借口，设置headers请求头
     if (options.url.indexOf("/my/") !== -1) {
+        // if (options.url.includes("/my/")){
         options.headers = {
-
             Authorization: localStorage.getItem("token") || ""
         }
-    };
-
-
-    options.complete = function(res) {
-        //在complete回调函数中 可以用res.responseJSON拿到服务器响应回来的数据
-        console.log(res);
-        if (res.responseJSON.status !== 0 && res.responseJSON.message === "身份验证失败") {
-            localStorage.removeItem("token")
-            location.href = "/login.html"
-        }
     }
+
+    // options.complete = function(res) {
+    //     //在complete回调函数中 可以用res.responseJSON拿到服务器响应回来的数据
+    //     const { status, message } = res.responseJSON
+    //     if (status === 1 || message === "身份认证失败！") {
+    //         localStorage.removeItem("token")
+    //         location.href = "/login.html"
+    //     }
+    // }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,15 @@ $(function() {
         var layer = layui.layer
             //点击退出，实现退出功能
         $("#btnLogout").on("click", function() {
-            layer.confirm("确认退出吗", { icon: 3, title: "提示" }, function(index) {
+            layer.confirm("您确认退出吗", { icon: 3, title: "尊敬的VIP" }, function(index) {
                 //清除本地储存中的token
                 localStorage.removeItem("token")
                     //重新跳转到登录页面
                 location.href = "./login.html"
                     //关闭  confirm 询问框
                 layer.close(index)
+            }, function() {
+                return layer.msg("欢迎回来")
             })
         })
     })
@@ -26,14 +28,13 @@ function getUserInfo() {
             if (res.status !== 0) return layui.layer.msg("获取用户信息失败")
             renderAvater(res.data)
         },
-        // complete: function(res) {
-        //     //在complete回调函数中 可以用res.responseJSON拿到服务器响应回来的数据
-        //     console.log(res);
-        //     if (res.responseJSON.status !== 0 && res.responseJSON.message === "身份验证失败") {
-        //         localStorage.removeItem("token")
-        //         location.href = "/login.html"
-        //     }
-        // }
+        complete: function(res) {
+            const { status, message } = res.responseJSON
+            if (status === 1 && message === "身份认证失败！") {
+                localStorage.removeItem("token")
+                location.href = "/login.html"
+            }
+        }
     })
 }
 
